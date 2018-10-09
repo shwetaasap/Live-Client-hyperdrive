01d6352bbf7fef91ca723c8220c1fd8cdafaf6b5
Shweta Agarwal <idshweta@gmail.com>
2018-10-09 11:18:45 +0200

    hyperdrive project

---
+++ app.js
@@ -0,0 +1,8 @@
+var list = document.querySelector('.output ul');
+var cities = ['lonDon', 'ManCHESTer', 'BiRmiNGHAM', 'liVERpoOL']; 
+ for(var i = 0; i < cities.length; i++) 
+ {  var input = cities[i]; 
+     var lower = input.toUpperCase(0); 
+     console.log("lower"); 
+ }
+ 
