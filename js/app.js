cb28d562950eac92d534fbf29388954276bf2415
Shweta Agarwal <idshweta@gmail.com>
2018-10-17 14:48:22 +0200

    final page

--- js/app.js
+++ js/app.js
@@ -1,18 +1,23 @@
 
-
-$(function(){
-    $("p").mouseover(function(){
+//shweta
+$(function () {
+    $("p").mouseover(function () {
         $("p").css("background-color", "lightgray");
+    });
+    $("p").mouseout(function () {
+        $("p").css("background-color", "white");
+    });
+    $(".velo1").mouseover(function () {
+        $("#velo1").slideDown().css("display", "block");
+    });
+    $(".velo1").mouseout(function () {
+        $("#velo1").slideDown().css("display", "none");
+    });
 });
-$("p").mouseout(function(){
-    $("p").css("background-color","white");
-});
-$(".velo1").mouseover(function(){
-    $("#velo1").slideDown().css("display","block");
-});
-$(".velo1").mouseout(function(){
-    $("#velo1").slideDown().css("display","none");
-});
+
+//garima
+$(document).ready(function () {
+    $("[data-toggle='tooltip']").tooltip();
 });
 
 
